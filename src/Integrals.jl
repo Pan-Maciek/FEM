cd1ca48ef1b19ff27de574686e3b4fc883046f2f
pan-maciek <kozieja.maciej@gmail.com>
2019-12-28 19:32:22 +0000

    integration methods

--- src/Integrals.jl
+++ src/Integrals.jl
@@ -4,21 +4,25 @@ include("Meshbuilder.jl")
 using Main.Integrals.Meshbuilder
 export ∫ds, ∬dS
 
-function simps(f, a, b, n=10)
+function simpson(f, a, b, n=10)
     iseven(n) || throw("n must be even, and $n was given")
-    h = (b - a) / n
-    s = f(a) + 
-        4sum(f.(a .+ collect(1:2:n) .* h)) + 
-        2sum(f.(a .+ collect(2:2:n-1) .* h)) + 
-    f(b)
-    h/3 * s
+    h = (b - a) // n
+    s = f(a) + 4sum(f.(a .+ (1:2:n) .* h)) + 2sum(f.(a .+ (2:2:n-1) .* h)) + f(b)
+    h//3 * s
 end
 
-∫ds(f :: Function, edge :: EdgeX) = (y = edge.xLine; simps(x -> f(x, y(x)), edge.xRange...))
-∫ds(f :: Function, edge :: EdgeY) = (x = edge.yLine; simps(y -> f(x(y), y), edge.yRange...))
-∫ds(f :: Function, mesh :: Mesh) = sum(∫ds.(f, mesh.edges))
+function trapezoid(f, a, b, n=10)
+    h = (b - a) // n
+    s = sum(f.(a .+ (0:n-1) .* h)) + sum(f.(a .+ (1:n) .* h))
+    h//2 * s
+end
 
-∬dS(f :: Function, rect :: Rectangle) = simps(x -> simps(y -> f(x, y), rect.yRange...), rect.xRange...)
-∬dS(f :: Function, mesh :: Mesh) = sum(∬dS.(f, mesh.grid))
+
+∫ds(f :: Function, edge :: EdgeX, int=simpson, n=10) = (y = edge.xLine; int(x -> f(x, y(x)), edge.xRange..., n))
+∫ds(f :: Function, edge :: EdgeY, int=simpson, n=10) = (x = edge.yLine; int(y -> f(x(y), y), edge.yRange..., n))
+∫ds(f :: Function, mesh :: Mesh,  int=simpson, n=10) = sum(∫ds.(f, mesh.edges, int, n))
+
+∬dS(f :: Function, rect :: Rectangle, int=simpson, n=10) = int(x -> int(y -> f(x, y), rect.yRange..., n), rect.xRange..., n)
+∬dS(f :: Function, mesh :: Mesh, int=simpson, n=10) = sum(∬dS.(f, mesh.grid, int, n))
 
 end
