07afe3deb94d3f6c48fbd8ab0958acaf4aba02b0
pan-maciek <kozieja.maciej@gmail.com>
2019-12-28 19:11:25 +0000

    removed unused code

--- src/Differentials.jl
+++ src/Differentials.jl
@@ -5,7 +5,6 @@ export ∂, ∂x, ∂y, ∂z
 const ϵ = 0.000000001
 
 incArg(X, n :: Number) = [i == n ? x + ϵ : x for (i, x) in enumerate(X)]
-setArg(X, n :: Number) = val -> [i == n ? val : x for (i, x) in enumerate(X)]
 
 ∂(f, n :: Number) = (X...) -> (Y = incArg(X, n); (f(Y...) .- f(X...)) ./ ϵ)
 ∂(f) = ∂(f, 1)
