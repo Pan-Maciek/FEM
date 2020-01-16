c3c13d03d194dd1982a18038fc58ee90426e2cb8
pan-maciek <kozieja.maciej@gmail.com>
2020-01-16 21:43:47 +0000

    mesh utils

--- src/Meshbuilder.jl
+++ src/Meshbuilder.jl
@@ -1,6 +1,5 @@
-include("Utils.jl")
 using Base.Iterators
-
+using PolygonOps
 abstract type Edge end
 struct EdgeX <: Edge
     xRange::Tuple{Number,Number}
@@ -21,87 +20,58 @@ function makeEdge(A::Vector, B::Vector)::Edge
     else EdgeY(yRange, lineY(A, B)) end
 end
 
-abstract type Shape end
-struct Rectangle <: Shape
+struct Rectangle
     xRange::Tuple{Number,Number}
     yRange::Tuple{Number,Number}
 end
 
 struct Mesh
-    grid::Vector{Shape}
     edges::Vector{Edge}
     points::Vector
     xStep::Number
     yStep::Number
 end
 
-wrap1(xs) = flatten((rest(poly, 2), [poly[1]]))
+function rectInPolygon(rect :: Rectangle, poly)
+    inpolygon([rect.xRange[1], rect.yRange[1]], poly) == 1 ||
+    inpolygon([rect.xRange[2], rect.yRange[2]], poly) == 1 ||
+    inpolygon([rect.xRange[1], rect.yRange[2]], poly) == 1 ||
+    inpolygon([rect.xRange[2], rect.yRange[1]], poly) == 1
+end
 
-makeGrid() = [
-    Rectangle((-1, 0), (-1, 0))
-    Rectangle((0, 1), (-1, 0))
-    Rectangle((-1, 0), (0, 1))
-]
+wrap1(xs) = flatten((rest(poly, 2), [poly[1]]))
+onEdge(point, edge::EdgeX) = abs(edge.xLine(point[1]) - point[2]) < 0.01 && (edge.xRange[1] < point[1] < edge.xRange[2])
+onEdge(point, edge::EdgeY) = abs(edge.yLine(point[2]) - point[1]) < 0.01 && (edge.yRange[1] < point[2] < edge.yRange[2])
 
 function makeGrid(poly, edges, ΓN, resolution)
     xMin, yMin = min.(poly...)
     xMax, yMax = max.(poly...)
 
     grid = falses(resolution + 1, resolution + 1)
+    edges = edges[[x == 0 for x = ΓN]]
 
     xStep = (xMax - xMin) / resolution
     yStep = (yMax - yMin) / resolution
-    x2index(x, r) :: Int = round((x - xMin) / xStep, r) + 1
-    y2index(y, r) :: Int = round((y - yMin) / yStep, r) + 1
 
-    index2x(x) = xMin + (x - 1) * xStep
-    index2y(y) = yMin + (y - 1) * yStep
-
-    function fill(edge :: EdgeX, value)
-        local xMin, xMax = edge.xRange
-        for x in x2index(xMin, RoundUp):x2index(xMax, RoundDown)
-            y = y2index(edge.xLine(index2x(x)), RoundDown)
-            grid[x,y] = value
-        end
-    end
-    function fill(edge :: EdgeY, value)
-        local yMin, yMax = edge.yRange
-        for y in y2index(yMin, RoundUp):y2index(yMax, RoundDown)
-            x = x2index(edge.yLine(index2y(y)), RoundDown)
-            grid[x,y] = value
-        end
-    end
-
-    function isInside(x, y)
-        !grid[x,y] &&
-        findprev(grid[x,:], y) != nothing &&
-        findnext(grid[x,:], y) != nothing &&
-        findprev(grid[:,y], x) != nothing &&
-        findnext(grid[:,y], x) != nothing
-    end
-
-    fill.(edges, true)
     for x = 1:(resolution+1), y = 1:(resolution+1)
-        if isInside(x, y)
+        point = [xMin + (x - 1) * xStep, yMin + (y - 1) * yStep]
+        ip = inpolygon(point, poly)
+        if ip == 1
             grid[x,y] = true
+        elseif ip == -1
+            grid[x,y] = !any(map(edge -> onEdge(point, edge), edges))
         end
     end
-    fill.(edges[[!x for x in ΓN]], false)
-    @show grid
+
     for y = reverse(1:(resolution+1))
         println(grid[:,y])
     end
 
-   xStep, yStep, [
-        Rectangle((-1, 0), (-1, 0)),
-        Rectangle((0, 1), (-1, 0)),
-        Rectangle((0, 1), (0, 1))
-   ], [(xMin + (x - 1) * xStep, yMin + (y - 1) * yStep) for x=1:(resolution+1), y=1:(resolution+1) if grid[x,y]]
+   xStep, yStep, [(xMin + (x - 1) * xStep, yMin + (y - 1) * yStep) for x=1:(resolution+1), y=1:(resolution+1) if grid[x,y]][[true, true, true, true, false, false, true, false]]
 end
 
-# mesh = makeMesh(poly, ΓN, 2)
 function makeMesh(poly, ΓN::Vector{Bool}, resolution :: Int) :: Mesh
     edges = makeEdge.(poly, wrap1(poly))
-    xStep, yStep, grid, points = makeGrid(poly, edges, ΓN, resolution)
-    Mesh(grid, edges[ΓN], points, xStep, yStep)
+    xStep, yStep, points = makeGrid(poly, edges, ΓN, resolution)
+    Mesh(edges[ΓN], points, xStep, yStep)
 end
