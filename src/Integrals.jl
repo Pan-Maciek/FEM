15151044d5ba054cb41dbaf6d931731c81f03207
pan-maciek <kozieja.maciej@gmail.com>
2020-01-16 21:38:32 +0000

    Gauss–Kronrod quadrature adaptive integration

--- src/Integrals.jl
+++ src/Integrals.jl
@@ -1,23 +1,11 @@
 include("Meshbuilder.jl")
-include("Utils.jl")
+using Cubature
 
-function simpson(f, a, b, n = 10)
-    iseven(n) || throw("n must be even, and $n was given")
-    h = (b - a) // n
-    s = f(a) + 4 * sum(f.(a .+ (1:2:n) .* h)) + 2 * sum(f.(a .+ (2:2:n-1) .* h)) + f(b)
-    h // 3 * s
-end
+∫∫ = hcubature
+∫ = hquadrature
 
-function trapezoid(f, a, b, n = 10)
-    h = (b - a) // n
-    s = sum(f.(a .+ (0:n-1) .* h)) + sum(f.(a .+ (1:n) .* h))
-    h // 2 * s
-end
+∫ds(f, edge::EdgeX) :: Number = (y = edge.xLine; ∫(x -> f(x, y(x)), edge.xRange...))[1]
+∫ds(f, edge::EdgeY) :: Number = (x = edge.yLine; ∫(y -> f(x(y), y), edge.yRange...))[1]
+∫ds(f, mesh::Mesh) :: Number = sum(∫ds.(f, mesh.edges))
 
-N = 100
-∫ds(f, edge::EdgeX, ∫ = simpson, n = N) :: Number = (y = edge.xLine; ∫(x -> f(x, y(x)), edge.xRange..., n))
-∫ds(f, edge::EdgeY, ∫ = simpson, n = N) :: Number = (x = edge.yLine; ∫(y -> f(x(y), y), edge.yRange..., n))
-∫ds(f, mesh::Mesh, ∫ = simpson, n = N) :: Number = sum(∫ds.(f, mesh.edges, ∫, n))
-
-∫∫dS(f, rect::Rectangle, ∫ = simpson, n = N) :: Number = ∫(x -> ∫(y -> f(x, y), rect.yRange..., n), rect.xRange..., n)
-∫∫dS(f, mesh::Mesh, ∫ = simpson, n = N) :: Number = sum(∫∫dS.(f, mesh.grid, ∫, n))
+∫∫dS(f, (xMin, xMax), (yMin, yMax)) :: Number = hcubature(v -> f(v...), [xMin, yMin], [xMax, yMax])[1]
