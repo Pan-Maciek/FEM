4b0021f1c4c7f0b08436a6d005c369bae8cdcbb2
Pan-Maciek <koziejka.com@gmail.com>
2020-01-19 02:49:50 +0100

    fixed base functions

--- src/main.jl
+++ src/main.jl
@@ -9,7 +9,8 @@ shape = [(-1, -1), (1, -1), (1, 0), (0, 0), (0, 1), (-1, 1)]
 Γ = [g; g; 0; 0; g; g]
 
 # base functions
-e(x0, y0) = (x, y) -> max(0, (1 - abs(x - x0) * dx) * (1 - abs(y - y0) * dy))
+e(x₀, y₀) = (x, y) -> (x₀ - dx, y₀ - dy) < (x, y) < (x₀ + dx, y₀ + dy)
+	? (1 - abs(x - x₀) * dx) * (1 - abs(y - y₀) * dy) : 0
 
 L(v) = ∫ds(k * g * v)
 B(u, v) = ∫∫dS(k * (∂x(u) * ∂x(v) + ∂y(u) * ∂y(v)))
